d9db08058177857dd83079ff20181960758c6691
xiaxiaxiaxiaxiaruo <ruojingzhang@163.com>
2017-03-08 11:02:19 +0800

    test

---
+++ README.txt
@@ -0,0 +1,1 @@
+2017spring software testing
---
+++ test.cpp
@@ -0,0 +1,27 @@
+/*********************************************************
+    > File Name: test.cpp
+  > Author: Jing
+  > Mail: 
+  > Created Time: 三  3/ 8 10:59:01 2017
+ ***********************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<queue>
+#include<stack>
+using namespace std;
+
+const int N=100;
+const int inf=0x3f3f3f3f;
+
+int main(){
+
+ return 0;
+}
